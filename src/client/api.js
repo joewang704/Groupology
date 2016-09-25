480baadd7f22a23ac874c1d2b9aec9efd1753ee0
joewang704 <joewang704@gmail.com>
2016-09-24 22:08:22 -0400

    Layout frontend

--- src/client/api.js
+++ src/client/api.js
@@ -10,37 +10,74 @@ function getData() {
     success: (data) => {
       console.log(data)
       const { lovers, participants, extremeTimePeople, density } = data
+      let max = -1
+      let maxParticipant
+      participants.forEach((user) => {
+        if (max < user.count) {
+          maxParticipant = user
+          max = user.count
+        }
+      })
       $('#container').prepend(`
+        <div class="title flex-row" style="order: -2">
+          Groupme Gay App.
+        </div>
+        <div class="flex-row" style="justify-content: space-around; width: 100%">
+          <div>
+            <div class="user center">
+              LOVE BIRDS
+            </div>
+            <div class="flex-row">
+              <div>
+                <div class="icon-mask">
+                  <img class="icon-resize" src="${lovers[0].img}"></img>
+                </div>
+                <div class="center">${lovers[0].name}</div>
+              </div>
+              <div class="heart" style="margin: 20px;">
+                <i class="heart fa fa-heart" aria-hidden="true"></i>
+              </div>
+              <div>
+                <div class="icon-mask">
+                  <img class="icon-resize" src="${lovers[1].img}"></img>
+                </div>
+                <div class="center">${lovers[1].name}</div>
+              </div>
+            </div>
+          </div>
         <div>
-          <div class="title flex-row">
-            Groupme Gay App.
+          <div class="user center">
+            EARLY BIRD
           </div>
-          <div class="flex-row">
-            <div>
-              <div>${lovers[0].name}</div>
-              <div class="icon-mask">
-                <img class="icon-resize" src="${lovers[0].img}"></img>
-              </div>
-            </div>
-            <div class="heart">
-              <i class="heart fa fa-heart" aria-hidden="true"></i>
-            </div>
-            <div>
-              <div>${lovers[1].name}</div>
-              <div class="icon-mask">
-                <img class="icon-resize" src="${lovers[1].img}"></img>
-              </div>
-            </div>
+          <div class="icon-mask">
+            <img class="icon-resize" src="${extremeTimePeople.earlyBird.img}"></img>
           </div>
-          <div class="flex-row">
-            <div>
-              Early Bird:
-              ${extremeTimePeople.earlyBird.name}
-            </div>
-            <div>
-              Night Owl:
-              ${extremeTimePeople.nightOwl.name}
-            </div>
+          <div class="center">
+            ${extremeTimePeople.earlyBird.name}
+          </div>
+        </div>
+        <div>
+          <div class="user center">
+            NIGHT OWL
+          </div>
+          <div class="icon-mask">
+            <img class="icon-resize" src="${extremeTimePeople.nightOwl.img}"></img>
+          </div>
+          <div class="center">
+            ${extremeTimePeople.nightOwl.name}
+          </div>
+        </div>
+      `)
+      $('#firstRow').prepend(`
+        <div class="flex-column">
+          <div class="user">
+            MOST TALKATIVE
+          </div>
+          <div class="icon-mask">
+            <img class="icon-resize" src="${maxParticipant.image_url}"></img>
+          </div>
+          <div>
+            ${maxParticipant.nickname}
           </div>
         </div>
       `)
