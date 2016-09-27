c3f0bfd7d42601845a23273c9c302be97a7eac29
joewang704 <joewang704@gmail.com>
2016-09-27 16:04:31 -0400

    Code touchup

--- src/server/api.js
+++ src/server/api.js
@@ -1,12 +1,8 @@
 const request = require('request-promise')
 
-//const token = 'e24d3770640f0134076a19766f95137f'
-//const token = '08355a20649b0134076a19766f95137f'
-
 const getMessages = (token, groupId) =>
   request({
     uri: `https://api.groupme.com/v3/groups/${groupId}/messages`,
-    //uri: 'https://api.groupme.com/v3/groups/13817068/messages',
     qs: {
       token,
       limit: 100,
@@ -17,7 +13,6 @@ const getMessages = (token, groupId) =>
 const getMembers = (token, groupId) =>
   request({
     uri: `https://api.groupme.com/v3/groups/${groupId}`,
-    //uri: 'https://api.groupme.com/v3/groups/13817068',
     qs: {
       token,
     },
